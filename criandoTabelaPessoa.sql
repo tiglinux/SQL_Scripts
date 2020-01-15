fee868fa1872f263561f06a60b54a847ff22a37a
Tiago Ribeiro Santos <tiago.programador@hotmail.com>
2020-01-15 11:05:20 -0300

    Create criandoTabelaPessoa.sql

---
+++ criandoTabelaPessoa.sql
@@ -0,0 +1,12 @@
+
+create database testeEstudosSpring;
+use testeEstudosSpring;
+
+create table Pessoa(
+	cod_id INT(50) PRIMARY KEY NOT NULL,
+    nome VARCHAR(60) NOT NULL,
+    idade INT(2) NOT NULL,
+    sobrenome VARCHAR(60) NOT NULL
+);
+    
+    
